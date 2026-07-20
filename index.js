36605b71a4aff678a2a38d68b8f24c3fd4b2d18c
nicolascanas <ig.contato.nico@gmail.com>
2026-07-20 19:33:16 -0300

    aula-17: os tipos primitivos

--- index.js
+++
@@ -1,8 +0,0 @@
-const price = 30;
-
-price = 40
-
-console.log(price);
-
-// let: Permite mudar o valor da variável depois.
-// const: O valor inicial é fixo. Não muda.
---
+++ secao-02-dia-01/15-constantes/15-constantes-js
@@ -0,0 +1,8 @@
+const price = 30;
+
+price = 40
+
+console.log(price);
+
+// let: Permite mudar o valor da variável depois.
+// const: O valor inicial é fixo. Não muda.
